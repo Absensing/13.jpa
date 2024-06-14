cd76981037613c6ee6b73d23244400e02666ebc3
jaejae <santoki389@gmail.com>
2024-06-14 17:33:52 +0900

    modify, delete

---
+++ chap06-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/entity/Menu.java
@@ -0,0 +1,98 @@
+package com.ohgiraffers.springdatajpa.menu.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+@Table(name = "tbl_menu")
+/* 2. @Builder lombok 라이브러리에서 제공해주는 빌더 사용 */
+//@Builder(toBuilder = true)
+public class Menu {
+
+    @Id
+    @Column(name = "menu_code")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int menuCode;
+
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @Column(name = "menu_price")
+    private int menuPrice;
+
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "orderable_status")
+    private String orderableStatus;
+
+    /* 3. Entity 클래스 내부에서 builder 패턴 구현 */
+    public Menu MenuName(String var) {
+        this.menuName = var;
+        return this;
+    }
+
+    public Menu MenuPrice(int var) {
+        this.menuPrice = var;
+        return this;
+    }
+
+    public Menu CategoryCode(int var) {
+        this.categoryCode = var;
+        return this;
+    }
+
+    public Menu OrderableStatus(String var) {
+        this.orderableStatus = var;
+        return this;
+    }
+
+    public Menu builder() {
+        return new Menu(menuCode, menuName, menuPrice, categoryCode, orderableStatus);
+    }
+
+    protected Menu() {}
+
+    public Menu(int menuCode, String menuName, int menuPrice, int categoryCode, String orderableStatus) {
+        this.menuCode = menuCode;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.categoryCode = categoryCode;
+        this.orderableStatus = orderableStatus;
+    }
+
+    public int getMenuCode() {
+        return menuCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuCode=" + menuCode +
+                ", menuName='" + menuName + '\'' +
+                ", menuPrice=" + menuPrice +
+                ", categoryCode=" + categoryCode +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+
+//    public void setMenuName(String menuName) {
+//        this.menuName = menuName;
+//    }
+}
